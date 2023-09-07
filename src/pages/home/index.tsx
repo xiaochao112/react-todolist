c419e5a046766730f6ea73a99e82bf3a79a7f852
myGre <1745083253@qq.com>
2023-09-07 20:30:22 +0800

    添加过渡动画

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { getTaskList } from '@api/task';
 import { useStateUserInfo } from '@store/hook';
 import { TTaskItem } from '@api/task/type';
+import { Spin } from 'antd';
 
 export type TSaerchParams = {
   // taskType: number;
@@ -19,14 +20,19 @@ function MyHome() {
   const [taskList, setTaskList] = useState<TTaskItem[]>([]);
   const [page, setPage] = useState({ page: 1, pageSize: 10 });
   const [searchParams, setSearchParams] = useState<TSaerchParams>();
+  const [isFetching, setIsFetching] = useState(false);
   // 初始化请求
   useEffect(() => {
     let ignore = false;
+    setIsFetching(true);
     const getList = async () => {
       if (searchParams) {
         const data = { ...page, ...searchParams };
         if (userInfo.isLogin) {
           const res = await getTaskList(data);
+          setTimeout(() => {
+            setIsFetching(false);
+          }, 200);
           // 如果有缓存，则不重新赋值
           if (!ignore) {
             setTaskList(res.result!.result);
@@ -56,7 +62,9 @@ function MyHome() {
           }}
         />
         <div className='content flex w-full justify-center'>
-          <Content taskList={taskList} getList={getList} searchTime={searchParams!} />
+          <Spin tip='Loading...' spinning={isFetching}>
+            <Content taskList={taskList} getList={getList} searchTime={searchParams!} />
+          </Spin>
         </div>
       </div>
     </>
